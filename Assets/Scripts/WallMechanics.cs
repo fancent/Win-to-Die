f176acab76f4d977e7d554ace2795a6b7f8b1ad3
fancent <vincent.kc.fan@gmail.com>
2020-01-30 19:24:18 -0500

    - added physics material - added horizontal bounce -> still need to add acceleration

---
+++ Assets/Scripts/WallMechanics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMechanics : MonoBehaviour
+{
+    public float pushForce = 10;
+    public Rigidbody rb;
+ 
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+    // Update is called once per frame
+    void Update() {
+ 
+    }
+ 
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Right Wall") {
+            Debug.Log("pushLeft");
+            rb.AddForce(Vector3.left * pushForce, ForceMode.Impulse);
+        }
+        else if(other.gameObject.tag == "Left Wall") {
+            Debug.Log("pushRight");
+            rb.AddForce(Vector3.right * pushForce, ForceMode.Impulse);
+        }
+    }
+}
