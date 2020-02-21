190a43deb8499166aa8b6c7c245718c867721521
Brian Nguyen <nguyen.brian70@gmail.com>
2020-02-21 01:59:07 -0500

    Rewrite WallMechanics targets

--- Assets/Scripts/WallMechanics.cs
+++ Assets/Scripts/WallMechanics.cs
@@ -6,29 +6,32 @@ public class WallMechanics : MonoBehaviour
 {
     public float pushForce = 10;
     public Rigidbody rb;
- 
+
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
     }
- 
-    // Update is called once per frame
-    void Update() {
- 
-    }
- 
-    void OnCollisionEnter(Collision other) {
-        GameSystem g_sys = GameObject.Find("GameSystem").GetComponent<GameSystem>();
-        
-        g_sys.speedUp(other.gameObject.tag, 1.05f);
-        if(other.gameObject.tag == "Right Wall") {
-            Debug.Log("pushLeft");
-            rb.AddForce((Vector3.left) * pushForce, ForceMode.Impulse);
-        }
-        else if(other.gameObject.tag == "Left Wall") {
-            Debug.Log("pushRight");
-            rb.AddForce((Vector3.right) * pushForce, ForceMode.Impulse);
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameSystem g_sys = GameObject.Find("GameSystem").GetComponent<GameSystem>();
+        g_sys.speedUp(other.gameObject.tag, 1.05f);
+        if (this.gameObject.tag == "Right Wall")
+        {
+            Debug.Log("pushLeft");
+            other.gameObject.GetComponent<Rigidbody>().AddForce((Vector3.left) * pushForce, ForceMode.Impulse);
+        }
+        else if (this.gameObject.tag == "Left Wall")
+        {
+            Debug.Log("pushRight");
+            other.gameObject.GetComponent<Rigidbody>().AddForce((Vector3.right) * pushForce, ForceMode.Impulse);
         }
     }
 }
