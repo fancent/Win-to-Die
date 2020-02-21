be6977c1c53fbb1823747e89c893eea0290338e1
Brian Nguyen <nguyen.brian70@gmail.com>
2020-02-21 00:33:16 -0500

    Add right wall generator code

--- Assets/PathCreator/Examples/Scripts/RoadMeshCreator.cs
+++ Assets/PathCreator/Examples/Scripts/RoadMeshCreator.cs
@@ -52,6 +52,7 @@ namespace PathCreation.Examples
                 AssignMaterials();
                 CreateRoadMesh();
                 CreateLeftWall();
+                CreateRightWall();
             }
         }
 
@@ -174,12 +175,8 @@ namespace PathCreation.Examples
                 Vector3 localUp = (usePathNormals) ? Vector3.Cross(path.GetTangent(i), path.GetNormal(i)) : path.up;
                 Vector3 localRight = (usePathNormals) ? path.GetNormal(i) : Vector3.Cross(localUp, path.GetTangent(i));
 
-                // // Find position to left and right of current path vertex
-
-                // Vector3 vertSideA = path.GetPoint(i) - localRight * Mathf.Abs(wallWidth) - localRight + localUp * thickness;
-                // Vector3 vertSideB = path.GetPoint(i) + localRight * Mathf.Abs(roadWidth) - localRight - localRight + localUp * thickness;
-
-                Vector3 vertSideA = path.GetPoint(i) - localRight * Mathf.Abs(wallWidth) - localRight + localUp * thickness;
+                // Find position to left and right of current path vertex
+                Vector3 vertSideA = path.GetPoint(i) - localRight * Mathf.Abs(wallWidth + roadWidth) + localUp * thickness;
                 Vector3 vertSideB = path.GetPoint(i) - localRight * Mathf.Abs(roadWidth) + localUp * thickness;
 
                 // Add top of road vertices
@@ -240,6 +237,97 @@ namespace PathCreation.Examples
             leftWallMesh.SetTriangles(underRoadTriangles, 1);
             leftWallMesh.SetTriangles(sideOfRoadTriangles, 2);
             leftWallMesh.RecalculateBounds();
+        }
+
+        void CreateRightWall()
+        {
+            Vector3[] verts = new Vector3[path.NumPoints * 8];
+            Vector2[] uvs = new Vector2[verts.Length];
+            Vector3[] normals = new Vector3[verts.Length];
+
+            int numTris = 2 * (path.NumPoints - 1) + ((path.isClosedLoop) ? 2 : 0);
+            int[] roadTriangles = new int[numTris * 3];
+            int[] underRoadTriangles = new int[numTris * 3];
+            int[] sideOfRoadTriangles = new int[numTris * 2 * 3];
+
+            int vertIndex = 0;
+            int triIndex = 0;
+
+            // Vertices for the top of the road are layed out:
+            // 0  1
+            // 8  9
+            // and so on... So the triangle map 0,8,1 for example, defines a triangle from top left to bottom left to bottom right.
+            int[] triangleMap = { 0, 8, 1, 1, 8, 9 };
+            int[] sidesTriangleMap = { 4, 6, 14, 12, 4, 14, 5, 15, 7, 13, 15, 5 };
+
+            bool usePathNormals = !(path.space == PathSpace.xyz && flattenSurface);
+            for (int i = 0; i < path.NumPoints; i++)
+            {
+                Vector3 localUp = (usePathNormals) ? Vector3.Cross(path.GetTangent(i), path.GetNormal(i)) : path.up;
+                Vector3 localRight = (usePathNormals) ? path.GetNormal(i) : Vector3.Cross(localUp, path.GetTangent(i));
+
+                // Find position to left and right of current path vertex
+                Vector3 vertSideA = path.GetPoint(i) + localRight * Mathf.Abs(roadWidth) + localUp * thickness;
+                Vector3 vertSideB = path.GetPoint(i) + localRight * Mathf.Abs(wallWidth + roadWidth) + localUp * thickness;
+
+                // Add top of road vertices
+                verts[vertIndex + 0] = vertSideA;
+                verts[vertIndex + 1] = vertSideB;
+                // Add bottom of road vertices
+                verts[vertIndex + 2] = vertSideA - localUp * thickness;
+                verts[vertIndex + 3] = vertSideB - localUp * thickness;
+
+                // Duplicate vertices to get flat shading for sides of road
+                verts[vertIndex + 4] = verts[vertIndex + 0];
+                verts[vertIndex + 5] = verts[vertIndex + 1];
+                verts[vertIndex + 6] = verts[vertIndex + 2];
+                verts[vertIndex + 7] = verts[vertIndex + 3];
+
+                // Set uv on y axis to path time (0 at start of path, up to 1 at end of path)
+                uvs[vertIndex + 0] = new Vector2(0, path.times[i]);
+                uvs[vertIndex + 1] = new Vector2(1, path.times[i]);
+
+                // Top of road normals
+                normals[vertIndex + 0] = localUp;
+                normals[vertIndex + 1] = localUp;
+                // Bottom of road normals
+                normals[vertIndex + 2] = -localUp;
+                normals[vertIndex + 3] = -localUp;
+                // Sides of road normals
+                normals[vertIndex + 4] = -localRight;
+                normals[vertIndex + 5] = localRight;
+                normals[vertIndex + 6] = -localRight;
+                normals[vertIndex + 7] = localRight;
+
+                // Set triangle indices
+                if (i < path.NumPoints - 1 || path.isClosedLoop)
+                {
+                    for (int j = 0; j < triangleMap.Length; j++)
+                    {
+                        roadTriangles[triIndex + j] = (vertIndex + triangleMap[j]) % verts.Length;
+                        // reverse triangle map for under road so that triangles wind the other way and are visible from underneath
+                        underRoadTriangles[triIndex + j] = (vertIndex + triangleMap[triangleMap.Length - 1 - j] + 2) % verts.Length;
+                    }
+                    for (int j = 0; j < sidesTriangleMap.Length; j++)
+                    {
+                        sideOfRoadTriangles[triIndex * 2 + j] = (vertIndex + sidesTriangleMap[j]) % verts.Length;
+                    }
+
+                }
+
+                vertIndex += 8;
+                triIndex += 6;
+            }
+
+            rightWallMesh.Clear();
+            rightWallMesh.vertices = verts;
+            rightWallMesh.uv = uvs;
+            rightWallMesh.normals = normals;
+            rightWallMesh.subMeshCount = 3;
+            rightWallMesh.SetTriangles(roadTriangles, 0);
+            rightWallMesh.SetTriangles(underRoadTriangles, 1);
+            rightWallMesh.SetTriangles(sideOfRoadTriangles, 2);
+            rightWallMesh.RecalculateBounds();
         }
 
         void setupMeshHolder(GameObject holder)
