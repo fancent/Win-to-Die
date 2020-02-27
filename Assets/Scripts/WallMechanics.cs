c36a327ab9e0a821db31622aad41a24ac561b65f
Kyurem1001 <33588498+Kyurem1001@users.noreply.github.com>
2020-02-27 14:30:30 -0500

    Working but not really

--- Assets/Scripts/WallMechanics.cs
+++ Assets/Scripts/WallMechanics.cs
@@ -34,21 +34,30 @@ public class WallMechanics : MonoBehaviour
         // }
         // Debug.Log("Hit wall");
         // Debug.Log(wallside);
-        
+
         WheelVehicle w = other.gameObject.GetComponent<WheelVehicle>();
         // Debug.Log(w.gameObject);
         Vector3 norm = -other.GetContact(0).normal.normalized;
-        Vector3 indir =   (other.GetContact(0).point) - (w.transform.position);
-        Vector3 dir =(indir - 2*Vector3.Dot(indir, norm)*norm);
-        dir = (new Vector3(dir.x, -0.6f* dir.y, dir.z)).normalized;
+        Vector3 indir = (other.GetContact(0).point) - (w.transform.position);
+        Vector3 dir = (indir - 2 * Vector3.Dot(indir, norm) * norm);
+        Vector3 fwdF = (wallside ? Quaternion.Euler(0, -90, 0) * norm : Quaternion.Euler(0, 90, 0) * norm);
+        float co = 1f * w._rb.velocity.y * w._rb.velocity.y / (w._rb.velocity.x * w._rb.velocity.x + w._rb.velocity.z * w._rb.velocity.z);
+        fwdF.y = Mathf.Pow(co * (fwdF.x * fwdF.x + fwdF.z * fwdF.z), 0.3f);
+        if (fwdF.y > 0)
+            fwdF.y *= -1;
+        dir.y = Mathf.Pow(co * (dir.x * dir.x + dir.z * dir.z), 0.5f);
+        if (dir.y > 0)
+            dir.y *= -1;
         Debug.Log(w);
         Debug.Log(norm);
         Debug.Log(other.GetContact(0).point);
         Debug.Log(w.transform.position);
         Debug.Log(indir);
         Debug.Log(dir);
-        if(w == null)
+        if (w == null)
             return;
-        w.Cart_speedup(dir, 1.9f* Mathf.Pow(Vector3.Distance(Vector3.zero, w._rb.velocity), 0.3f));
+        w._rb.rotation = Quaternion.LookRotation(fwdF.normalized);
+        w.Cart_speedup(12f * dir + 0.5f* fwdF, 0.7f);
+        w.Cart_speedup(2.5f * fwdF, Mathf.Pow(Vector3.Distance(Vector3.zero, w._rb.velocity), 0.25f) * 1.5f);
     }
 }
