2b6f2cc535141f9a83ff59fa98e5e4a2fc6d7fa8
fancent <vincent.kc.fan@gmail.com>
2020-02-27 13:42:08 -0500

    - new camera scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform camLocation;
+    public Transform lookLocation;
+    public float Speed = 10.0f;
+ 
+    void FixedUpdate() {
+        transform.position = Vector3.Lerp(transform.position, camLocation.position, Speed * Time.deltaTime);
+        transform.LookAt(lookLocation.position);
+    }
+     
+}
