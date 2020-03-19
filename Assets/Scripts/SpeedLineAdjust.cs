cc8a0098a76a08e1ebbd41d045e41b103c118500
Kyurem1001 <33588498+Kyurem1001@users.noreply.github.com>
2020-03-19 16:22:50 -0400

    Improvement

---
+++ Assets/Scripts/SpeedLineAdjust.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VehicleBehaviour;
+
+public class SpeedLineAdjust : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem p;
+    public WheelVehicle v;
+    ParticleSystem.EmissionModule e;
+    void Start()
+    {
+        e = p.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        e.rateOverTime = v.Speed/10f;
+    }
+}
---
+++ Assets/Scripts/SpeedMeter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using VehicleBehaviour;
+public class SpeedMeter : MonoBehaviour
+{
+    public WheelVehicle v;
+    public Text t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.text = $"{(int)(v.Speed)} km/h";
+    }
+}
